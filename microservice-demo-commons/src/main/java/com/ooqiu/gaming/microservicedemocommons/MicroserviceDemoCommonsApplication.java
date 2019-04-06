a303efe158cab2ccba28b7ad099481c0a87c203c
Cheysen <1457516535@qq.com>
2019-04-06 17:37:48 +0800

    first change

---
+++ microservice-demo-commons/src/main/java/com/ooqiu/gaming/microservicedemocommons/MicroserviceDemoCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.ooqiu.gaming.microservicedemocommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDemoCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDemoCommonsApplication.class, args);
+    }
+
+}
---
+++ microservice-demo-dependencies/src/main/java/com/ooqiu/gaming/microservice/demo/dependencies/MicroserviceDemoDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.ooqiu.gaming.microservice.demo.dependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDemoDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDemoDependenciesApplication.class, args);
+    }
+
+}
